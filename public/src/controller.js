8d3b7a9b46749e9f8cbaf23f5025b3e9a6d73d1b
Erica Peharda <ericapeharda@gmail.com>
2015-06-01 16:24:51 -0600

    added ordering and fields to contacts page

--- public/src/controller.js
+++ public/src/controller.js
@@ -1,4 +1,13 @@
 angular.module('ContactsApp')
-.controller('ListController', function($scope){
-	$scope.contacts=[];
+.controller('ListController', function($scope, Contact){
+	//queries the server and returns the contacts 
+	$scope.contacts= Contact.query();
+	$scope.fields = ['firstName', 'lastName'];
+	$scope.sort = function(field){
+		$scope.sort.field = field;
+		$scope.sort.order = !$scope.sort.order; //will swap from false to true and vice versa
+	};
+
+	$scope.sort.field = 'firstName';
+	$scope.sort.order = false; //means descending
 });
